ea473f6ab5e61b7db13c766b743730760adaa44c
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-11-18 21:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("src", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var src = document.getElementById(ev.dataTransfer.getData("src"));
+    var srcParent = src.parentNode;
+    var tgt = ev.currentTarget.firstElementChild;
+
+    ev.currentTarget.replaceChild(src, tgt);
+    srcParent.appendChild(tgt);
+}
